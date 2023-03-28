8b5170f2ea9d6ecd1a9eba8a6242a63fdc769c13
Martin Lysell <martin.lysell@accenture.com>
2023-03-28 14:37:16 +0200

    Update deploy-addon script to be more verbose

--- .github/actions/rafay-deploy/deploy-addon.sh
+++ .github/actions/rafay-deploy/deploy-addon.sh
@@ -5,12 +5,11 @@
 ## Configure the bash runtime
 set -o nounset
 set -o pipefail
-#set -x
 
 ## Setup temp workspace with exit trap cleanup
 readonly workspace="$(mktemp -d -t addon.XXXXXX)"
 function __delete_workspace() {
-    [[ -d "${workspace}" ]] &&  echo rm -rf "${workspace}"
+    [[ -d "${workspace}" ]] &&  rm -rf "${workspace}"
 }
 trap __delete_workspace EXIT
 
@@ -78,6 +77,8 @@ if [[ -z "${addon_spec_file}" ]]; then
         exit 1
     fi
 
+    log "Generating addon spec from input variables ..."
+
     ## Construct the YQ command string that will create the YAML. Add the artifact
     ## to right place depending on content type (Helm vs plain YAML)
     yq_string='.apiVersion = "infra.k8smgmt.io/v3" | .kind = "Addon"'
@@ -98,7 +99,7 @@ if [[ -z "${addon_spec_file}" ]]; then
     fi
     
     ## Run YQ to generate the addon yaml
-    yq -n "${yq_string}" > "${addon_spec_file}"    
+    yq -n "${yq_string}" > "${addon_spec_file}"
 else
     if [[ ! -f "${addon_spec_file}" ]]; then
         log "Specified spec file does not exist: ${addon_spec_file}"
@@ -115,8 +116,10 @@ if [[ -z $(yq '.spec.artifact.artifact.repository // ""' "${addon_spec_file}") ]
     ## If the specified chart path exist and is a directory
     ## package the chart and update the manifest to point to the package
     if [[ -d "${chart_path}" ]]; then
-        helm package --dependency-update --destination "${workspace}" $(dirname "${addon_spec_file}")/"${chart_path}"
+        log "Packageing helm chart into tgz archive ..."
+        helm package --dependency-update --destination "${workspace}" $(dirname "${addon_spec_file}")/"${chart_path}" | log_indent
         
+        log "Updating spec file to use packaged chart ..."
         cp "${workspace}"/*.tgz $(dirname "${addon_spec_file}")/helm-chart.tgz
         yq -i '.spec.artifact.artifact.chartPath.name = "file://helm-chart.tgz"' "${addon_spec_file}"
     fi
@@ -128,17 +131,22 @@ if [[ -z $(yq '.spec.artifact.artifact.repository // ""' "${addon_spec_file}") ]
     ## (This is to paper over that the current client can only handle one file)
     if [[ ! -z "${yaml_paths}" ]]; then
         combined_yaml_file="${workspace}/combined.yaml"
+
+        log "Merging all specified yaml manifests into one file ..."
+
         while IFS='' read item; do
             item_path="$(dirname "${addon_spec_file}")/${item#file://}"
 
             ## If item is a regular file add it to the combined file
             if [[ -f "${item_path}" ]]; then
+                log_indent "+ ${item_path}"
                 echo -e "---\n## Source: ./${item#file://}" >> "${combined_yaml_file}"
                 cat "${item_path}" >> "${combined_yaml_file}"
             
             ## If item is a directory, scan it and add all yaml files
             elif [[ -d "${item_path}" ]]; then
                 while IFS='' read filespec; do
+                    log_indent "+ ${item_path}"
                     echo -e "---\n## Source: ${filespec}" >> "${combined_yaml_file}"
                     cat "${item_path}/${filespec}" >> "${combined_yaml_file}"
                 done < <(cd "${item_path}/"; find . -type f "(" -iname "*.yaml" -or -iname "*.yml" ")" | sort)
@@ -151,21 +159,35 @@ if [[ -z $(yq '.spec.artifact.artifact.repository // ""' "${addon_spec_file}") ]
         done <<< "${yaml_paths}"
 
         ## Copy in the combined file and add it to the spec file
+        log "Updating spec file to use merged yaml bundle ..."
         cp "${combined_yaml_file}" $(dirname "${addon_spec_file}")/
         yq -i '.spec.artifact.artifact.paths = [{"name" : "file://combined.yaml"}]' "${addon_spec_file}"
     fi
 fi
 
 ## Update addon parameters if overrides are specified
-[[ ! -z "${addon_name:-}" ]] && yq -i '.metadata.name = "'${addon_name}'"' "${addon_spec_file}"
-[[ ! -z "${addon_project:-}" ]] && yq -i '.metadata.project = "'${addon_project}'"' "${addon_spec_file}"
-[[ ! -z "${addon_namespace:-}" ]] && yq -i '.spec.namespace = "'${addon_namespace}'"' "${addon_spec_file}"
-[[ ! -z "${addon_version:-}" ]] && yq -i '.spec.version = "'${addon_version}'"' "${addon_spec_file}"
+log "Updating spec fields from input ..."
+if [[ ! -z "${addon_name:-}" ]]; then
+    log_indent '.metadata.name = "'${addon_name}'"'
+    yq -i '.metadata.name = "'${addon_name}'"' "${addon_spec_file}"
+fi
+[[ ! -z "${addon_project:-}" ]]; then
+    log_indent '.metadata.project = "'${addon_project}'"'
+    yq -i '.metadata.project = "'${addon_project}'"' "${addon_spec_file}"
+fi
+[[ ! -z "${addon_namespace:-}" ]]; then
+    log_indent '.spec.namespace = "'${addon_namespace}'"'
+    yq -i '.spec.namespace = "'${addon_namespace}'"' "${addon_spec_file}"
+fi
+[[ ! -z "${addon_version:-}" ]]; then
+    log_indent '.spec.version = "'${addon_version}'"'
+    yq -i '.spec.version = "'${addon_version}'"' "${addon_spec_file}"
+fi
 
-echo "-- Rendered Spec File --"
-cat ${addon_spec_file}
-echo "----------------------"
-
+## Run rctl to make the deploy API call
+log "Deploying addon ..."
+RCTL_PROJECT="defaultproject" \
 RCTL_API_KEY="${rafay_api_key}" \
 RCTL_API_SECRET="${rafay_api_key}" \
-rctl create addon version --v3 -f "${addon_spec_file}"
+rctl create addon version --v3 -f "${addon_spec_file}" | log_indent
+log "all done."
